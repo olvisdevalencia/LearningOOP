3a415d41330ed463358d8a34cc9abc0d5a845195
Desktop <Desktop@190.205.105.92>
2014-08-10 03:22:48 -0430

    Otro commit ya listo para dormir :)

--- index.php
+++ index.php
@@ -19,3 +19,4 @@ echo " es decir que puede hacer unos  cuantos cambios aqui antes de modificar el
 echo 'y luego poder unirlo con el que esta en el github y de forma viceversa :D'; // probando
 
 //como que no puedo darle merge cuando quiera :S
+//Modificando de nuevo
