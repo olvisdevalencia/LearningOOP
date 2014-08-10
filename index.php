896da71f76e7dd492062063eff18933eacb00694
Olvis Quintana <olvisdevalencia@hotmail.com>
2014-08-10 03:20:01 -0430

    Update index.php

--- index.php
+++ index.php
@@ -17,3 +17,5 @@ echo " probando un  tercer intento de commit ;)";
 echo " es decir que puede hacer unos  cuantos cambios aqui antes de modificar el de github y guardarlo en github tambien";
 
 echo 'y luego poder unirlo con el que esta en el github y de forma viceversa :D'; // probando
+
+//como que no puedo darle merge cuando quiera :S
